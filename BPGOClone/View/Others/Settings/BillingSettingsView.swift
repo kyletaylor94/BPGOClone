a586897dbd959a291932f91760d167a76d1b043f
T. Csaba <kyletaylorrules94@gmail.com>
2025-01-19 21:12:43 +0100

    added settingsoptions

---
+++ BPGOClone/View/Others/Settings/BillingSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  BillingSettingsView.swift
+//  BPGOClone
+//
+//  Created by Turdesan Csaba on 2025. 01. 19..
+//
+
+import SwiftUI
+
+struct BillingSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BillingSettingsView()
+}
